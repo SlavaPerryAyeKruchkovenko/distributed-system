ec81804fe9dd16cc3ff6f0c97b71a0aaf131191f
slava <v.kruchkovenko@tk-chel.ru>
2023-10-27 19:12:37 +0500

    Initial commit

---
+++ src/test/kotlin/com/example/springdistributedsystem/SpringDistributedSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdistributedsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDistributedSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
