1e9b856c994f7d4a4930e8f468b5ec52855109ed
slava <v.kruchkovenko@tk-chel.ru>
2023-11-15 19:29:55 +0500

    add rebbitmq

---
+++ src/main/kotlin/com/example/springdistributedsystem/config/RabbitMQConfig.kt
@@ -0,0 +1,45 @@
+package com.example.springdistributedsystem.config
+
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.core.BindingBuilder
+import org.springframework.amqp.core.DirectExchange
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitAdmin
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class RabbitMQConfig {
+    @Value("\${rabbitmq.username}")
+    private lateinit var username: String
+
+    @Value("\${rabbitmq.password}")
+    private lateinit var password: String
+
+    @Bean
+    fun connectionFactory(): ConnectionFactory {
+        val connectionFactory = CachingConnectionFactory("localhost")
+        connectionFactory.username = username
+        connectionFactory.setPassword(password)
+        return connectionFactory
+    }
+
+    @Bean
+    fun amqpAdmin(): AmqpAdmin {
+        return RabbitAdmin(connectionFactory())
+    }
+
+    @Bean
+    fun rabbitTemplate(): RabbitTemplate {
+        return RabbitTemplate(connectionFactory())
+    }
+
+    @Bean
+    fun exchange(): DirectExchange {
+        return DirectExchange("testExchange", true, false)
+    }
+}
