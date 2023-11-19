35f4aa79cffa156ddf17f0aeeb47d7a198433f54
Slava <kruchkovenko@gmail.com>
2023-11-19 13:06:29 +0500

    add consumer

--- app/src/main/kotlin/com/example/springdistributedsystem/model/errors/ApiError.kt
+++
@@ -1,3 +0,0 @@
-package com.example.springdistributedsystem.model.errors
-
-data class ApiError(val fields: Map<String,String?>,val global: List<String?>)
--- app/src/main/kotlin/com/example/springdistributedsystem/repository/LinkRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.springdistributedsystem.repository
-
-import com.example.springdistributedsystem.model.entity.LinkEntity
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface LinkRepository : CrudRepository<LinkEntity, Long>
