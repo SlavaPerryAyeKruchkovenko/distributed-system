b6fbdca003b8aad81ea3ab7808693317b6a49869
slava <v.kruchkovenko@tk-chel.ru>
2023-11-21 11:18:59 +0500

    fix errors

--- link-consumer/src/main/kotlin/com/example/springdistributedsystem/linkconsumer/consumer/LinkConsumer.kt
+++ link-consumer/src/main/kotlin/com/example/springdistributedsystem/linkconsumer/consumer/LinkConsumer.kt
@@ -5,9 +5,10 @@ import com.example.springdistributedsystem.linkconsumer.config.RabbitConfigurati
 import com.example.springdistributedsystem.linkconsumer.model.Link
 import com.example.springdistributedsystem.linkconsumer.model.UpdateLinkStatus
 import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.client.toEntity
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
 
 @Component
@@ -19,18 +20,39 @@ class LinkConsumer {
             webClient.get()
                 .uri(link.url)
                 .retrieve()
-                .toEntity(Void::class.java)
-                .subscribe {
-                    changeLinkStatus(webClient, link.id, it.statusCode.value())
+                .onStatus({
+                    it.is4xxClientError || it.is5xxServerError
+                }) { response ->
+                    response.bodyToMono(String::class.java).flatMap { body ->
+                        Mono.error(
+                            WebClientResponseException.create(
+                                response.statusCode().value(),
+                                response.statusCode().toString(),
+                                HttpHeaders.EMPTY,
+                                body.toByteArray(),
+                                null
+                            )
+                        )
+                    }
                 }
-            println(link)
+                .toBodilessEntity()
+                .subscribe({
+                    changeLinkStatus(webClient, link.id, it.statusCode.value())
+                    println("change link status ${it.statusCode}")
+                }, {
+                    if (it is WebClientResponseException) {
+                        changeLinkStatus(webClient, link.id, it.statusCode.value())
+                    } else {
+                        changeLinkStatus(webClient, link.id, 500)
+                    }
+                })
         } catch (e: Exception) {
             changeLinkStatus(webClient, link.id, 500)
         }
     }
 
     fun changeLinkStatus(client: WebClient, id: Long, status: Int) {
-        val appUrl = "http://app:3000/api/DistributedSystem/v1/links/{id}/status"
+        val appUrl = "http://nginx/api/DistributedSystem/v1/links/{id}/status"
         val updateStatus = BodyInserters.fromValue(UpdateLinkStatus(status))
         client.patch()
             .uri(appUrl, id)
