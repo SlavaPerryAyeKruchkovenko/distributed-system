89d5aaf8f2489ccf9214cde0756f0ba78811303c
Slava <kruchkovenko@gmail.com>
2023-11-29 00:11:54 +0500

    dix service

--- link-consumer/src/main/kotlin/com/example/springdistributedsystem/linkconsumer/service/LinkService.kt
+++ link-consumer/src/main/kotlin/com/example/springdistributedsystem/linkconsumer/service/LinkService.kt
@@ -13,37 +13,30 @@ import reactor.core.publisher.Mono
 import java.util.concurrent.TimeUnit
 
 @Service
-class LinkService(private val redisTemplate: RedisTemplate<String, Int>) {
+class LinkService {
     private val webClient = WebClient.create()
 
     @Cacheable("urlStatusCache")
     fun getStatusByUrl(url: String): Int {
-        val cacheStatus = redisTemplate.opsForValue().get(url)
-        if (cacheStatus != null) {
-            return cacheStatus
-        } else {
-            val status = try {
-                val response = webClient.get().uri(url).retrieve().onStatus({
-                    it.is4xxClientError || it.is5xxServerError
-                }) { response ->
-                    response.bodyToMono(String::class.java).flatMap { body ->
-                        Mono.error(
-                            WebClientResponseException.create(
-                                response.statusCode().value(),
-                                response.statusCode().toString(),
-                                HttpHeaders.EMPTY,
-                                body.toByteArray(),
-                                null
-                            )
+        return try {
+            val response = webClient.get().uri(url).retrieve().onStatus({
+                it.is4xxClientError || it.is5xxServerError
+            }) { response ->
+                response.bodyToMono(String::class.java).flatMap { body ->
+                    Mono.error(
+                        WebClientResponseException.create(
+                            response.statusCode().value(),
+                            response.statusCode().toString(),
+                            HttpHeaders.EMPTY,
+                            body.toByteArray(),
+                            null
                         )
-                    }
-                }.toBodilessEntity().block()
-                response?.statusCode?.value() ?: 500
-            } catch (e: Exception) {
-                500
-            }
-            redisTemplate.opsForValue().set(url, status, 10, TimeUnit.MINUTES)
-            return status
+                    )
+                }
+            }.toBodilessEntity().block()
+            response?.statusCode?.value() ?: 500
+        } catch (e: Exception) {
+            500
         }
     }
 
